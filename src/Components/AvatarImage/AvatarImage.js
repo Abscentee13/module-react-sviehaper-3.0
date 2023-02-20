ca08a5c0818d18ff9faf59f73d131c4e3b522c91
NSviehaper <svihaper@gmail.com>
2023-02-20 18:50:42 +0200

    Start UserInfo

---
+++ src/Components/AvatarImage/AvatarImage.js
@@ -0,0 +1,12 @@
+import css from './Avatar.module.css';
+
+
+const AvatarImage = ({avatarUrl}) => {
+
+    return (
+            <img src={avatarUrl} alt="Avatar" className={css.avatarImage}/>
+    );
+
+};
+
+export {AvatarImage};
---
+++ src/themes/theme.js
@@ -0,0 +1,56 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const lightTheme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            contrastText: "#fff",
+            dark: "rgb(59, 71, 59)",
+            light: "rgb(119, 132, 119)",
+            main: "#556655"
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        tomato: '#FF6347',
+        pink: {
+            deep: '#FF1493',
+            hot: '#FF69B4',
+            medium: '#C71585',
+            pale: '#DB7093',
+            light: '#FFB6C1',
+        },
+    },
+
+
+    components: {
+        MuiTypography: {
+            defaultProps: {
+                variantMapping: {
+                    h1: 'h5',
+                    h2: 'h5',
+                    h3: 'h5',
+                    h4: 'h5',
+                    h5: 'h5',
+                    h6: 'h5',
+                    subtitle1: 'h3',
+                    subtitle2: 'h3',
+                    body1: 'span',
+                    body2: 'span',
+                },
+            },
+        },
+    },
+});
+
+export const darkTheme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#90caf9',
+        },
+        secondary: {
+            main: '#556655',
+        },
+    },
+});
